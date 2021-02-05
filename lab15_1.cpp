3f229a189a09c6e99ac8f25e1f8ec07b0c8c75cd
tananun <tananun_c@cmu.ac.th>
2021-02-05 17:23:59 +0700

    F

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i); 
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double a[0],int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = rand()%101/100.00;
+    }
+}
+void findRowSum(const double *data,double *sum,int N,int M){
+    
+    for(int i = 0; i < N; i++){
+        *(sum+i) = 0;
+        for(int j = 0; j < M; j++ ){
+        *(sum+i) += *(data+i*M+j); 
+
+        }
+    }
+}
+
+void findColSum(const double *data,double *sum,int N,int M){
+    for(int k=0;k<M;k++){
+    *(sum+k) = 0;
+    }
+    for(int i=0; i<N ;i++){
+        for(int j=0; j<M;j++){
+        *(sum+j) += *(data+(i*M)+j);
+        
+        }
+}
+
+
+}
